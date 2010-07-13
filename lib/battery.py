1aa557af7a409beff3cb5a67853185e034146614
Odin Dutton <odindutton@gmail.com>
2010-07-13 18:51:52 +1000

    Prompt: - added collapsible pwd function - added current rvm info - updated battery indicator

--- lib/battery.py
+++ lib/battery.py
@@ -25,8 +25,11 @@ charge_threshold = int(math.ceil(10 * charge))
 # Output
 
 total_slots, slots = 10, []
-filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'▸'
-empty = (total_slots - len(filled)) * u'▹'
+
+filled_count = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'▸'
+
+filled = int(math.ceil(charge_threshold * (total_slots / 100.0))) * u'⚡'
+empty = (total_slots - 10) * u'▹'
 
 out = (filled + empty).encode('utf-8')
 import sys
@@ -36,8 +39,8 @@ color_yellow = '%{[1;33m%}'
 color_red = '%{[31m%}'
 color_reset = '%{[00m%}'
 color_out = (
-    color_green if len(filled) > 6
-    else color_yellow if len(filled) > 4
+    color_green if len(filled_count) > 6
+    else color_yellow if len(filled_count) > 4
     else color_red
 )
 
