a0b9506ca794d94244264b1faf267dbfb9e6f6b1
Odin Dutton <odindutton@gmail.com>
2010-07-15 13:51:30 +1000

    Fixed dotfiles load order - Also: - Updated readme - Added to the hints file - Cleaned up zsh completions file

---
+++ shell/exports.sh
@@ -0,0 +1,7 @@
+# Exports
+export CDPATH=".:$HOME:$HOME/Sites:$HOME/Sites/Mocra" # Search /Sites and /Sites/Mocra folder on 'cd' command 
+export EDITOR='mate -w' # Set textmate as default editor (-w is to wait for Textmate window to close)
+export ARCHFLAGS='-arch x86_64'
+export PATH=$PATH:/usr/local/mysql/bin # For mysql commands
+export PATH="/Users/twe4ked/.homebrew/bin":$PATH # For homebrew
+export PATH=/Library/PostgreSQL8/bin:$PATH # For pg gem
