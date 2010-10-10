3db224158687a7aa9a1a509ce35888bee7c4af7a
twe4ked <odindutton@gmail.com>
2010-10-10 12:52:32 +1000

    Added config options

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,7 +1,12 @@
 # Exports
-export CDPATH=".:$HOME:$HOME/Sites:$HOME/Sites/Mocra" # Search /Sites and /Sites/Mocra folder on 'cd' command 
+#
+# The WORK variable is set in ~/.dotfiles/config
+#
+export CDPATH=".:$HOME:$HOME/Sites:$HOME/Sites/$WORK" # Search /Sites and /Sites/$WORK folder on 'cd' command 
 export EDITOR='mate -w' # Set textmate as default editor (-w is to wait for Textmate window to close)
-export ARCHFLAGS='-arch x86_64'
-export PATH=$PATH:/usr/local/mysql/bin # For mysql commands
-export PATH="$HOME/.homebrew/bin":$PATH # For homebrew
-export PATH=/Library/PostgreSQL8/bin:$PATH # For pg gem
+export ARCHFLAGS='-arch x86_64'             # Archflags
+
+# Path settings
+export PATH=$PATH:/usr/local/mysql/bin      # For mysql commands
+export PATH="$HOME/.homebrew/bin":$PATH     # For homebrew
+export PATH=/Library/PostgreSQL8/bin:$PATH  # For pg gem
