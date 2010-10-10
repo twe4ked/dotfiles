dd78144f99e2d1831c997d4fdee73112b7063862
twe4ked <odindutton@gmail.com>
2010-10-10 12:53:40 +1000

    Updated install script

--- bin/install.sh
+++ bin/install.sh
@@ -8,10 +8,10 @@
 #   better let me know or fork, fix and submit a pull request.
 # 
 # Usage:
-# bash < <( curl http://github.com/twe4ked/dotfiles/raw/master/bin/install.sh )
+#   bash < <( curl http://github.com/twe4ked/dotfiles/raw/master/bin/install.sh )
 
 if [[ -d "$HOME/.dotfiles" ]]; then
-  echo "The dotfiles are already "
+  echo "The dotfiles are already on your computer!"
 else
   echo "Cloning twe4ked's dotfiles repo to ~/.dotfiles"
   cd ~/
@@ -21,15 +21,22 @@ else
   cd ~/
   ln -s ~/.dotfiles/lib/irbrc .irbrc # Setup .irbrc symlink
   
-  echo "Installing oh-my-zsh"
-  cd ~/
+  # wget $1 -O $tempfile
   wget http://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+  if [[ $? != 0 ]]; then
+    echo "Wget error or Wget not installed"
+    # rm $tempfile
+    exit
+  else
+    echo "Installing oh-my-zsh"
+    cd ~/
+  fi
   
-  echo ""
+  echo "Creating symlink to custom oh-my-zsh theme"
   cd ~/.oh-my-zsh/themes
   ln -s ~/.dotfiles/zsh/zsh-theme my.zsh-theme # Setup my.zsh-theme symlink (command must be run from ~/.oh-my-zsh/themes)
   
-  echo "Removing .zshrc and replacing with a custom version"
+  echo "Removing .zshrc and replacing with a symlink to a custom version"
   echo "Old version will be renamed .zshrc.back"
   cd ~/
   cp .zshrc .zshrc.back
