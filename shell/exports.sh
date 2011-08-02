62fcf363f1f75640c85394fce489f129ee248373
Odin Dutton <odindutton@gmail.com>
2011-08-02 22:28:24 +1000

    Remove $DOTFILES/bin from the PATH.

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,7 +1,4 @@
 # Exports
-#
-# The WORK variable is set in ~/.dotfiles/config
-#
 export CDPATH=".:$HOME:$HOME/Sites:$HOME/Sites/$WORK" # Search /Sites and /Sites/$WORK folder on 'cd' command 
 export EDITOR='vim'                         # Set vim as the default editor
 export ARCHFLAGS='-arch x86_64'             # Archflags
@@ -10,7 +7,6 @@ export GREP_OPTIONS='--color=auto'          # Double rainbow!
 # Path settings
 export PATH=$PATH:/usr/local/mysql/bin      # For mysql commands
 export PATH=$HOME/.homebrew/bin:$PATH       # For homebrew
-export PATH=$PATH:$DOTFILES/bin             #
 export PATH=$PATH:/var/lib/gems/1.8/bin
 export PATH=$PATH:/Library/Ruby/Gems/1.8
 export PATH=$PATH:/Users/twe4ked/.gem/ruby/1.8
