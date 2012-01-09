d2b55522889a8093763d48d9e4ae6f2bf5f9997f
Odin Dutton <odindutton@gmail.com>
2012-01-09 21:06:12 +1000

    Whitespace.

--- shell/colours.sh
+++ shell/colours.sh
@@ -2,7 +2,7 @@
 function colour {
   local reset='\e[0m'
   local white='\e[1;37m'
-  
+
   # Main colours
   local black='\e[0;30m'
   local blue='\e[0;34m'
@@ -12,7 +12,7 @@ function colour {
   local purple='\e[0;35m'
   local yellow='\e[1;33m'
   local gray='\e[0;30m'
-  
+
   # Light colours
   local light_blue='\e[1;34m'
   local light_green='\e[1;32m'
@@ -21,6 +21,6 @@ function colour {
   local light_purple='\e[1;35m'
   local light_yellow='\e[0;33m'
   local light_gray='\e[0;37m'
-  
-  printf "$(eval echo \$$1)" 
+
+  printf "$(eval echo \$$1)"
 }
