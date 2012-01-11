8ba2576c9c6ad6bc66b6f34a0eae1ca01a9552d7
Odin Dutton <odindutton@gmail.com>
2012-01-11 15:20:18 +1000

    Set the window title and tab title.
    Window title: <user>@<hostname>: <cwd> <command>
    Tab title:    <cwd_basename> <command>

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -108,3 +108,15 @@ function note_prompt {
 chpwd() {
   note_prompt
 }
+
+# Appears at the beginning of (and during) of command execution
+function termsupport_preexec {
+  emulate -L zsh
+  setopt extended_glob
+  local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}  # cmd name only, or if this is sudo or ssh, the next cmd
+  print -Pn "\e]2;%n@%M: %~ $CMD\a"       # add the current command to the window title
+  print -Pn "\e]1;%1~ $CMD\a"             # add the current command to the terminal tab
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook preexec termsupport_preexec
