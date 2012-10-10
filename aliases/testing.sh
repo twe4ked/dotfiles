fdec292415becf75ed58f5ae05133a5b078a3ef6
Odin Dutton <odindutton@gmail.com>
2012-10-10 10:50:57 +1000

    Switch to @jasoncodes testing aliases.

--- aliases/testing.sh
+++ aliases/testing.sh
@@ -1,10 +1,10 @@
 # Cucumber
 alias bec='bundle exec cucumber --drb'
+alias bec='CUCUMBER_FORMAT=fuubar $([ -S .zeus.sock ] && echo zeus || echo bundle exec) cucumber $([ -S .zeus.sock ] || echo --drb)'
 
 # RSpec
-alias ber='bundle exec rspec --drb'
+alias ber='$([ -S .zeus.sock ] && echo zeus || echo bundle exec) $(egrep -q "^ {4}rails \(2\." Gemfile.lock && echo spec --format=nested --colour || echo rspec --format=$(egrep -q fuubar Gemfile.lock && echo Fuubar || echo doc)) $([ -S .zeus.sock ] || echo --drb)'
 alias berd='ber --format=doc'
-alias berf='ber --format=Fuubar'
 
 # Spork
 alias spork='bundle exec spork'
