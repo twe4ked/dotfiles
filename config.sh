829206b3e0a23e16a36e1b61b16793c92f91fe46
Odin Dutton <odindutton@gmail.com>
2012-11-02 21:11:03 +1000

    Remove Terminal.app resume directory code.

--- config.sh
+++ config.sh
@@ -10,17 +10,5 @@ if [ $SHELL = "/bin/zsh" ]; then
   source $DOTFILES/zsh/config.sh
 fi
 
-# Set Apple Terminal.app resume directory
-# http://superuser.com/questions/313650/resume-zsh-terminal-os-x-lion/315029#315029
-if [[ $TERM_PROGRAM == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] {
-  function chpwd {
-    local SEARCH=' '
-    local REPLACE='%20'
-    local PWD_URL="file://$HOSTNAME${PWD//$SEARCH/$REPLACE}"
-    printf '\e]7;%s\a' $PWD_URL
-  }
-  chpwd
-}
-
 eval "$(rbenv init - | grep -v \^rbenv\ rehash)"
 gem() { rbenv exec gem "$@" && rbenv rehash && hash -r; }
