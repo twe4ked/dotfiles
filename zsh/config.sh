090deed63e24c9315f7cc2764a3c12dae457129a
Odin Dutton <odindutton@gmail.com>
2012-11-05 21:50:33 +1000

    Handle git flow completion file with fresh.

--- zsh/config.sh
+++ zsh/config.sh
@@ -4,7 +4,6 @@ source $DOTFILES/zsh/prompt.sh      # load my EPIC prompt
 
 # vendor
 source $DOTFILES/zsh/vendor/syntax_highlighting.zsh
-source $DOTFILES/zsh/vendor/git_flow_completion.zsh
 source $DOTFILES/zsh/vendor/substring_search.zsh
 
 setopt prompt_subst                 # allows variable substitution in the prompt
