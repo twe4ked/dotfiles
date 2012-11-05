a2e9fac1af76aea8d9a5d57a8022fe930b1e0763
Odin Dutton <odindutton@gmail.com>
2012-11-05 16:01:47 +1000

    Fresh all the rest of the things!

--- install.sh
+++ install.sh
@@ -3,31 +3,7 @@
 cd
 [ -d .dotfiles ] || git clone git://github.com/twe4ked/dotfiles.git .dotfiles
 
-function create_link()
-{
-  local SRC="$1"
-  local DST="$2"
-
-  if [ ! -e "$DST" ]
-  then
-    ln -sv "$SRC" "$DST"
-  else
-    if [ ! -L "$DST" ] || [ "`readlink "$DST"`" != "$SRC" ]
-    then
-      echo -n "dotfiles: $DST already exists" >&2
-      if [ -L "$DST" ]
-      then
-        echo " (pointing to `readlink "$DST"`)"
-      else
-        echo " (not a symlink)"
-      fi
-    fi
-  fi
-}
-
-mkdir -p ~/.vim
-
-create_link ~/.dotfiles/lib/freshrc ~/.freshrc
+[ -e ~/.freshrc ] || ln -sv ~/.dotfiles/lib/freshrc ~/.freshrc
 
 if [ -e ~/.fresh/build/shell.sh ]; then
   source ~/.fresh/build/shell.sh
