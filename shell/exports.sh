69d0779a4602216d777c66a779d489cd38c716d5
Odin Dutton <odindutton@gmail.com>
2012-11-05 16:19:01 +1000

    Use @jasoncodes $EDITOR exports with fresh.

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,5 +1,3 @@
-export EDITOR='mvim'                        # Set mvim as the default editor
-export GIT_EDITOR='vim'                     # Use terminal Vim for git
 export ARCHFLAGS='-arch x86_64'             # Archflags
 export GREP_OPTIONS='--color=auto'          # Double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # Path settings
