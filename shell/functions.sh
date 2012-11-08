0084b21f23b05ae27b7ac88335abfdef0780872b
Odin Dutton <odindutton@gmail.com>
2012-11-08 21:34:01 +1000

    Remove functions already in fresh.

--- shell/functions.sh
+++ shell/functions.sh
@@ -19,19 +19,3 @@ hitch() {
   if [[ -s "$HOME/.hitch_export_authors" ]] ; then source "$HOME/.hitch_export_authors" ; fi
 }
 alias unhitch='hitch -u'
-
-function __database_yml {
-  if [[ -f config/database.yml ]]; then
-    ruby -ryaml -rerb -e "puts YAML::load(ERB.new(IO.read('config/database.yml')).result)['${RAILS_ENV:-development}']['$1']"
-  fi
-}
-
-function psql
-{
-  if [[ "$(__database_yml adapter)" == 'postgresql' ]]; then
-    PGDATABASE="$(__database_yml database)" "$(/usr/bin/which psql)" "$@"
-    return $?
-  fi
-  "$(/usr/bin/which psql)" "$@"
-}
-export PSQL_EDITOR='vim +"set syntax=sql"'
