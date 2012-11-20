bea8134d12704184eb340f3ecbdb0e3693f3ce8b
Odin Dutton <odindutton@gmail.com>
2012-11-21 09:00:16 +1000

    Extend `gh` function to open issues/pulls/wiki pages.

--- aliases/github.sh
+++ aliases/github.sh
@@ -1,3 +1,17 @@
 function gh() {
-  open "https://$(git config remote.origin.url | sed 's/\.git//' | sed 's/https:\/\///' | sed 's/git:\/\///' | sed 's/git@//' | tr ':' '/')"
+  local PROJECT="https://$(git config remote.origin.url | sed 's/\.git//' | sed 's/https:\/\///' | sed 's/git:\/\///' | sed 's/git@//' | tr ':' '/')"
+  case "$1" in
+    i|issues|-i|--issues)
+      open "$PROJECT/issues"
+      ;;
+    p|pulls|-p|--pulls)
+      open "$PROJECT/pulls"
+      ;;
+    w|wiki|-w|--wiki)
+      open "$PROJECT/pulls"
+      ;;
+    *)
+      open "$PROJECT"
+      ;;
+  esac
 }
