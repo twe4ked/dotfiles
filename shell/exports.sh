d84fda225e428353675c5697571089ceabf5d780
Odin Dutton <odindutton@gmail.com>
2012-11-27 10:39:54 +1000

    Move $DOTFILES variable into shell/exports.sh.

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,3 +1,4 @@
+export DOTFILES=~/.dotfiles
 export ARCHFLAGS='-arch x86_64'             # Archflags
 export GREP_OPTIONS='--color=auto'          # Double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # Path settings
