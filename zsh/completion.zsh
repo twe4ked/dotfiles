9f4615c063d7d5808dec0bb21b69209a9a30407c
Odin Dutton <odindutton@gmail.com>
2012-12-08 10:40:22 +1000

    Clean up Zsh completion file.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,34 +1,28 @@
 autoload -U compinit; compinit
 
-# Group matches and describe groups
+# group matches and describe groups
 zstyle ':completion:*:matches' group 'yes'
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*:descriptions' format $'\e[01;33m-- %d --\e[0m'            # Group group description format
-zstyle ':completion:*:warnings' format $'\e[01;31m-- No Matches Found --\e[0m'  # Message shown when there are no completion matches
-
-zstyle ':completion:*:functions' ignored-patterns '_*'                          # Don't auto-complete internal functions (ones prefixed with _)
+zstyle ':completion:*:descriptions' format $'\e[01;33m-- %d --\e[0m'            # group group description format
+zstyle ':completion:*:warnings' format $'\e[01;31m-- No Matches Found --\e[0m'  # message shown when there are no completion matches
+zstyle ':completion:*:functions' ignored-patterns '_*'                          # don't auto-complete internal functions (ones prefixed with _)
 zstyle ':completion::complete:cd:' tag-order local-directories path-directories
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
 
-# Disable named-directories autocompletion
+# disable named-directories autocompletion
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
-# Load known hosts file for auto-completion with ssh and scp commands
+# load known hosts file for auto-completion with ssh and scp commands
 if [ -f ~/.ssh/known_hosts ]; then
   zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
   zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
 fi
 
-# Case-insensitive (all), partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-fi
+# case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 compinit -i
 zmodload -i zsh/complist
