db652abffee0458382c88d5c2a32645ca9ae4eb7
Odin Dutton <odindutton@gmail.com>
2012-12-17 11:08:09 +1000

    Hello? Are you still there?

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -4,6 +4,7 @@ alias sauce='reload'                          # Meta alias!
 alias history='fc -l 1'                       # I want to see more the 16 history items
 alias psg='ps aux | grep -v grep | grep $1'   # See what's running
 alias z="_z -r 2>&1"                          # Highest rated results first
+alias internet\?='ping 8.8.8.8'               # Google?
 
 # Open untracked/modified files in MacVim
 alias modified='$EDITOR $(git ls-files -m -o --exclude-standard) .'
