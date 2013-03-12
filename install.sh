a247eccb50e6cef9a29f64774eb78f63a39dad96
Odin Dutton <odindutton@gmail.com>
2013-03-12 17:32:16 +1000

    Fresh handles this symlink in it's install script.

--- install.sh
+++ install.sh
@@ -10,8 +10,6 @@ else
   git clone git://github.com/twe4ked/dotfiles.git ~/.dotfiles
 fi
 
-[ -e ~/.freshrc ] || ln -sv ~/.dotfiles/freshrc ~/.freshrc
-
 if [ -e ~/.fresh/build/shell.sh ]; then
   source ~/.fresh/build/shell.sh
   fresh
