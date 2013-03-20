f46848cbbcd3cc9a9cca427907c3cae568576101
Odin Dutton <odindutton@gmail.com>
2013-03-20 15:44:29 +1000

    Move zsh syntax highlighting config out of zshrc.

--- zsh/zsh-syntax-highlighting.zsh
+++ zsh/zsh-syntax-highlighting.zsh
@@ -1,1 +1,11 @@
 source ~/.fresh/build/vendor/zsh-syntax-highlighting.zsh
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+ZSH_HIGHLIGHT_STYLES+=(
+  unknown-token           'bg=red,bold'
+  alias                   'fg=magenta,bold'
+  single-quoted-argument  'fg=green,bold'
+  double-quoted-argument  'fg=green,bold'
+  builtin                 'fg=yellow,bold'
+  function                'fg=yellow,bold'
+)
