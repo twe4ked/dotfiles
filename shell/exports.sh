edb9dd4641e27aa83494d162ccc48eb147289f40
Odin Dutton <odindutton@gmail.com>
2013-04-07 16:12:52 +1000

    Add @pengwynn's git-go script

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,3 +2,4 @@ export DOTFILES=~/.dotfiles
 export ARCHFLAGS='-arch x86_64'             # Archflags
 export GREP_OPTIONS='--color=auto'          # Double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # Path settings
+export GH_LOGIN=twe4ked                     # Used by git-go
