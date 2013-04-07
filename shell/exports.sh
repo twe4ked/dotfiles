afaf3e3174aa89159eb188ce7188d3e74d6c7c99
Odin Dutton <odindutton@gmail.com>
2013-04-07 16:38:30 +1000

    Comments

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,5 +1,5 @@
-export DOTFILES=~/.dotfiles
-export ARCHFLAGS='-arch x86_64'             # Archflags
-export GREP_OPTIONS='--color=auto'          # Double rainbow!
-export PATH=$HOME/bin:/usr/local/bin:$PATH  # Path settings
-export GH_LOGIN=twe4ked                     # Used by git-go
+export DOTFILES=~/.dotfiles                 # here
+export ARCHFLAGS='-arch x86_64'             # archflags
+export GREP_OPTIONS='--color=auto'          # double rainbow!
+export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
+export GH_LOGIN=twe4ked                     # used by git-go
