689ad4eda3346c963589c69d1774c9030bf2671f
Odin Dutton <odindutton@gmail.com>
2013-04-07 15:29:06 +1000

    Remove leading space from irb prompt

--- config/irb/prompt.rb
+++ config/irb/prompt.rb
@@ -1,6 +1,6 @@
 IRB.conf[:PROMPT][:SIMPLE_COLOR] = {
-  :PROMPT_I => "#{ANSI[:BLUE]} »#{ANSI[:RESET]} ",
-  :PROMPT_N => "#{ANSI[:BLUE]} »#{ANSI[:RESET]} ",
+  :PROMPT_I => "#{ANSI[:BLUE]}»#{ANSI[:RESET]} ",
+  :PROMPT_N => "#{ANSI[:BLUE]}»#{ANSI[:RESET]} ",
   :PROMPT_C => "#{ANSI[:RED]}?»#{ANSI[:RESET]} ",
   :PROMPT_S => "#{ANSI[:YELLOW]}?»#{ANSI[:RESET]} ",
   :RETURN   => "#{ANSI[:GREEN]}=»#{ANSI[:RESET]} %s\n",
