25c7d3c609df06b2e202ffc2e6b5f3a2a03d5325
Odin Dutton <odindutton@gmail.com>
2013-04-11 12:52:06 +1000

    Clean up aliases file

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -1,15 +1,14 @@
-alias ls='ls -laGF'                           # Compact view, show color
-alias reload="source $HOME/.zshrc"            # Source dotfiles
-alias sauce='reload'                          # Meta alias!
-alias history='fc -l 1'                       # I want to see more the 16 history items
-alias z="_z -r 2>&1"                          # Highest rated results first
-alias internet\?='ping 8.8.8.8'               # Google?
+alias ls="ls -laGF"                           # compact view, show color
+alias reload="source $HOME/.zshrc"            # source dotfiles
+alias history="fc -l 1"                       # show more the 16 history items
+alias z="_z -r 2>&1"                          # highest rated results first
+alias internet\?="ping 8.8.8.8"               # google?
 
-alias b='bundle'                              # Run bundle
+alias b="bundle"                              # run bundle
 
-alias dev="cd $HOME/Dev"                      # Go to the Dev directory
-alias vimdir="cd $HOME/.dotfiles/vim"         # Go to the Vim directory
-alias dotdir="cd $HOME/.dotfiles"             # Go to the Dotfiles directory
+alias dev="cd $HOME/Dev"                      # go to the dev directory
+alias vimdir="cd $HOME/.dotfiles/vim"         # go to the vim directory
+alias dotdir="cd $HOME/.dotfiles"             # go to the dotfiles directory
 
 d() {
   if [[ -n "$1" ]]; then
