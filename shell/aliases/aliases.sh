44530f615f37460faa16b48e74c4e239d534e2c7
Odin Dutton <odindutton@gmail.com>
2013-08-19 16:35:01 +1000

    Human readable sizes in `ls`

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -1,4 +1,4 @@
-alias ls="ls -laGF"                           # compact view, show color
+alias ls="ls -laGFh"
 alias history="fc -l 1"                       # show more the 16 history items
 alias z="_z -r 2>&1"                          # highest rated results first
 alias internet\?="ping 8.8.8.8"               # google?
