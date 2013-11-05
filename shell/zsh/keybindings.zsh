63edc876b49a6da60d996a9e4ee337a4f95425b5
Odin Dutton <odindutton@gmail.com>
2013-11-05 20:43:57 +1000

    Quotes

--- shell/zsh/keybindings.zsh
+++ shell/zsh/keybindings.zsh
@@ -3,17 +3,17 @@ bindkey -v
 bindkey '^R' history-incremental-search-backward
 
 # option + right/left
-bindkey "\e\e[C" vi-forward-word
-bindkey "\e\e[D" vi-backward-word
+bindkey '\e\e[C' vi-forward-word
+bindkey '\e\e[D' vi-backward-word
 
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
+bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[B' history-beginning-search-forward
 
 # make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
 bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
+bindkey '^[[3~' delete-char
+bindkey '^[3;5~' delete-char
+bindkey '\e[3~' delete-char
 
 bindkey -M viins '^a' beginning-of-line
 bindkey -M viins '^e' end-of-line
