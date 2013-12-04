b92ef4b6eb8c880b6469a97b79c99bf2cfd717b1
Odin Dutton <odindutton@gmail.com>
2013-12-04 12:06:58 +1000

    Improve Zsh history keybindings

--- shell/zsh/keybindings.zsh
+++ shell/zsh/keybindings.zsh
@@ -7,8 +7,14 @@ bindkey '\e\e[C' vi-forward-word
 bindkey '\e\e[D' vi-backward-word
 
 # search history using up/down arrows
-bindkey '^[[A' history-beginning-search-backward
-bindkey '^[[B' history-beginning-search-forward
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "\e[A" history-beginning-search-backward-end
+bindkey "\e[B" history-beginning-search-forward-end
+
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
 
 # make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
 bindkey '^?' backward-delete-char
