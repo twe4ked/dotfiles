7170ade13da95fee74f6a831051f8564972c3dd0
Odin Dutton <odindutton@gmail.com>
2014-01-06 16:04:06 +1000

    Set GOPATH environment variable

--- shell/exports.sh
+++ shell/exports.sh
@@ -4,6 +4,10 @@ export GREP_OPTIONS='--color=auto'          # double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
 export GH_LOGIN=twe4ked                     # used by git-go
 
+if type foo >/dev/null 2>&1; then
+  export GOPATH="$(realpath `which go` | sed "s/\/libexec\/bin\/go//")"
+fi
+
 path_add() {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
     export PATH="${PATH:+"$PATH:"}$1"
