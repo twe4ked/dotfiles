fbae789b346129504866b4b735e4102721014547
Odin Dutton <odindutton@gmail.com>
2016-01-27 10:01:24 +1100

    Use ./bin/foreman if it exists

--- shell/aliases/ruby.sh
+++ shell/aliases/ruby.sh
@@ -1,3 +1,10 @@
 alias open_saved_page="find tmp/capybara | tail -1 | xargs open"
-alias fs='bundle exec foreman start'
 alias serve-ruby="ruby -run -e httpd . -p 5000"
+
+fs() {
+  if [[ -x ./bin/foreman ]]; then
+    ./bin/foreman start "$@"
+  else
+    foreman start "$@"
+  fi
+}
