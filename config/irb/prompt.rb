571362368a8afa3e0a3ab6fc59cef2a3b07d0ebf
Odin Dutton <odindutton@gmail.com>
2017-01-12 10:57:00 +1100

    Remove IRB config
    I want to keep IRB completely standard for testing things. Pry is nicely
    configured for normal use.

--- config/irb/prompt.rb
+++
@@ -1,8 +0,0 @@
-IRB.conf[:PROMPT][:SIMPLE_COLOR] = {
-  :PROMPT_I => "#{ANSI[:BLUE]}»#{ANSI[:RESET]} ",
-  :PROMPT_N => "#{ANSI[:BLUE]}»#{ANSI[:RESET]} ",
-  :PROMPT_C => "#{ANSI[:RED]}?»#{ANSI[:RESET]} ",
-  :PROMPT_S => "#{ANSI[:YELLOW]}?»#{ANSI[:RESET]} ",
-  :RETURN   => "#{ANSI[:GREEN]}=»#{ANSI[:RESET]} %s\n",
-  :AUTO_INDENT => true }
-IRB.conf[:PROMPT_MODE] = :SIMPLE_COLOR
