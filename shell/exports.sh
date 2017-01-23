f1faf809a99732f58c51bb1b0c3406ffee6ed39a
Odin Dutton <odindutton@gmail.com>
2017-01-23 16:16:47 +1100

    Only update homebrew on install once a week

--- shell/exports.sh
+++ shell/exports.sh
@@ -3,6 +3,7 @@ export ARCHFLAGS='-arch x86_64'             # archflags
 export GREP_OPTIONS='--color=auto'          # double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
 export GH_LOGIN=twe4ked                     # used by git-go
+export HOMEBREW_AUTO_UPDATE_SECS=604800     # 1 week in seconds
 
 path_add() {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
