163dabc39e5dc876b253f672dda737d4914387d5
Odin Dutton <odindutton@gmail.com>
2017-05-24 13:13:58 +1000

    Allow specifying processors in fs function

--- shell/aliases/ruby.sh
+++ shell/aliases/ruby.sh
@@ -2,9 +2,14 @@ alias open_saved_page="find tmp/capybara | tail -1 | xargs open"
 alias serve-ruby="ruby -run -e httpd . -p 5000"
 
 fs() {
+  INPUT="${@:-all}"
+  PROCS="$(echo $INPUT | sed "s/ /=1,/g")=1"
+
+  echo "foreman start -m $PROCS"
+
   if [[ -x ./bin/foreman ]]; then
-    ./bin/foreman start "$@"
+    ./bin/foreman start -m "$PROCS"
   else
-    foreman start "$@"
+    foreman start -m "$PROCS"
   fi
 }
