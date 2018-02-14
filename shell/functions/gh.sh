d1caf4dd9e1e74ee145d4d4a6ae815fda5e0bc43
Odin Dutton <odindutton@gmail.com>
2018-02-15 09:45:45 +1100

    Make `gh` handle GitHub URLs with extra characters at the end

--- shell/functions/gh.sh
+++ shell/functions/gh.sh
@@ -5,13 +5,19 @@ gh() {
   fi
 
   cd "$(
+    setopt KSH_ARRAYS BASH_REMATCH
     set -e
     set -o nounset
 
-    GH_URL="$1"
-    GH_PROJECT="$(basename "${GH_URL}")"
-    GH_USERNAME="$(basename "${GH_URL%/${GH_PROJECT}}")"
-    PROJECT_DIRECTORY="$PROJECTS/github/$GH_USERNAME/$GH_PROJECT"
+    if [[ $1 =~ "^https:\/\/github.com\/([^\/]*)\/([^\/]*)" ]]; then
+      GH_PROJECT="${BASH_REMATCH[2]}"
+      GH_USERNAME="${BASH_REMATCH[1]}"
+      PROJECT_DIRECTORY="$PROJECTS/github/$GH_USERNAME/$GH_PROJECT"
+    fi
+
+    if [[ $1 =~ "(^https:\/\/github.com\/[^\/]*\/[^\/]*)" ]]; then
+      GH_URL="${BASH_REMATCH[1]}"
+    fi
 
     if [[ ! -d "$PROJECT_DIRECTORY" ]]; then
       mkdir -p "$PROJECTS/github/$GH_USERNAME"
