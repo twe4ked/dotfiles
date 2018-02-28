e223e1008b120017dd837579995f21898c73f935
Odin Dutton <odindutton@gmail.com>
2018-02-28 15:08:56 +1100

    Allow gh to take a GitHub user/repo

--- shell/functions/gh.sh
+++ shell/functions/gh.sh
@@ -1,6 +1,6 @@
 gh() {
   if [[ $# -ne 1 ]]; then
-    echo "Usage gh <github-url>" 1>&2
+    echo "Usage gh [github url] [github user/repo]" 1>&2
     return 1
   fi
 
@@ -13,13 +13,15 @@ gh() {
       GH_PROJECT="${BASH_REMATCH[2]}"
       GH_USERNAME="${BASH_REMATCH[1]}"
       PROJECT_DIRECTORY="$PROJECTS/github/$GH_USERNAME/$GH_PROJECT"
-    fi
-
-    if [[ $1 =~ "(^https:\/\/github.com\/[^\/]*\/[^\/]*)" ]]; then
-      GH_URL="${BASH_REMATCH[1]}"
+    elif [[ $1 =~ "[^/]*\/[^/]*" ]]; then
+      PROJECT_DIRECTORY="$PROJECTS/github/$1"
     fi
 
     if [[ ! -d "$PROJECT_DIRECTORY" ]]; then
+      if [[ $1 =~ "(^https:\/\/github.com\/[^\/]*\/[^\/]*)" ]]; then
+        GH_URL="${BASH_REMATCH[1]}"
+      fi
+
       mkdir -p "$PROJECTS/github/$GH_USERNAME"
       git clone "$GH_URL" "$PROJECT_DIRECTORY"
     fi
