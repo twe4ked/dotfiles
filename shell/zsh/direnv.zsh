a169789c4cdb5982117269bf0599e8f86add182f
Odin Dutton <odindutton@gmail.com>
2018-12-19 10:02:51 +1100

    Add info about direnv fork

--- shell/zsh/direnv.zsh
+++ shell/zsh/direnv.zsh
@@ -1,1 +1,6 @@
+# NOTE: I'm currently running a fork of direnv to allow persisting "deny".
+#
+# https://github.com/direnv/direnv/pull/400
+# https://github.com/kalbasit/direnv/tree/direnv_persist-deny
+
 eval "$(direnv hook zsh)"
