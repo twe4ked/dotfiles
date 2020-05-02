bb23f0d4f35b5d4aaa5df57a31621186309896a2
Odin Dutton <odindutton@gmail.com>
2020-05-02 15:14:29 +1000

    Rename aurora_prompt file

--- shell/zsh/aurora_prompt.zsh
+++ shell/zsh/aurora_prompt.zsh
@@ -1,6 +1,6 @@
 setopt prompt_subst
 
-eval "$(prompt init "\
+eval "$(aurora_prompt init zsh "\
 {green}{cwd style=short}\
 {yellow} ± {git_branch}:{git_commit}{reset}\
 {dark_grey} {git_stash}{reset}\
