9b46d9423d292ce422b88cf145d520f339f6cea7
Odin Dutton <odindutton@gmail.com>
2020-05-02 15:18:30 +1000

    Remove unneeded {reset}s from aurora prompt config

--- shell/zsh/aurora_prompt.zsh
+++ shell/zsh/aurora_prompt.zsh
@@ -2,7 +2,7 @@ setopt prompt_subst
 
 eval "$(aurora_prompt init zsh "\
 {green}{cwd style=short}\
-{yellow} ± {git_branch}:{git_commit}{reset}\
-{dark_grey} {git_stash}{reset}\
-{dark_grey} {jobs}{reset}\
+{yellow} ± {git_branch}:{git_commit}\
+{dark_grey} {git_stash}\
+{dark_grey} {jobs}\
 {cyan} $ {reset}")"
