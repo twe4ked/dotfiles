a108870086b32fc9bf65f2742491e09a63080bfd
Odin Dutton <odindutton@gmail.com>
2021-04-12 15:17:57 +1000

    Remove direnv fork notes

--- shell/zsh/direnv.zsh
+++ shell/zsh/direnv.zsh
@@ -1,6 +1,1 @@
-# NOTE: I'm currently running a fork of direnv to allow persisting "deny".
-#
-# https://github.com/direnv/direnv/pull/400
-# https://github.com/kalbasit/direnv/tree/direnv_persist-deny
-
 eval "$(direnv hook zsh)"
