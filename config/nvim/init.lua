b10fe895f98ccd42abaab81e8e90d2a1cc3aa70b
Odin Dutton <odindutton@gmail.com>
2025-01-15 09:59:13 +1100

    Neovim search keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -408,14 +408,14 @@ require("lazy").setup(
 				local builtin = require("telescope.builtin")
 				vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 				vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-				vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+				vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Search [F]iles" })
 				vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 				vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 				vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 				vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 				vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 				vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-				vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+				vim.keymap.set("n", "<leader>l", builtin.buffers, { desc = "[l] Find existing buffers" })
 
 				-- Slightly advanced example of overriding default behavior and theme
 				-- vim.keymap.set("n", "<leader>/", function()
@@ -434,11 +434,6 @@ require("lazy").setup(
 						prompt_title = "Live Grep in Open Files",
 					})
 				end, { desc = "[S]earch [/] in Open Files" })
-
-				-- Shortcut for searching your Neovim configuration files
-				vim.keymap.set("n", "<leader>sn", function()
-					builtin.find_files({ cwd = vim.fn.stdpath("config") })
-				end, { desc = "[S]earch [N]eovim files" })
 			end,
 		},
 
