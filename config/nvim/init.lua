57f58e3728826547a268b6518b1351b6e7e12889
Odin Dutton <odindutton@gmail.com>
2025-01-21 13:48:15 +1100

    Use lightline.vim in Neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -974,21 +974,6 @@ require("lazy").setup(
 				-- - sr)'  - [S]urround [R]eplace [)] [']
 				require("mini.surround").setup()
 
-				-- Simple and easy statusline.
-				--  You could remove this setup call if you don't like it,
-				--  and try some other statusline plugin
-				local statusline = require("mini.statusline")
-				-- set use_icons to true if you have a Nerd Font
-				statusline.setup({ use_icons = vim.g.have_nerd_font })
-
-				-- You can configure sections in the statusline by overriding their
-				-- default behavior. For example, here we set the section for
-				-- cursor location to LINE:COLUMN
-				---@diagnostic disable-next-line: duplicate-set-field
-				statusline.section_location = function()
-					return "%2l:%-2v"
-				end
-
 				-- ... and there is more!
 				--  Check out: https://github.com/echasnovski/mini.nvim
 			end,
@@ -1060,6 +1045,9 @@ require("lazy").setup(
 
 		-- Directory viewer for Vim
 		"justinmk/vim-dirvish",
+
+		-- A light and configurable statusline/tabline plugin for Vim
+		"itchyny/lightline.vim",
 	},
 
 	---@diagnostic disable-next-line: missing-fields
